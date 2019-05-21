0dea2b2e0465c774f6b1f040c146da1d5c35bd91
cemal <cemalonder55@gmail.com>
2019-05-21 16:42:53 +0300

    added configuration param

--- src/com/apiomat/nativemodule/salesmodule71/SalesModule71.java
+++ src/com/apiomat/nativemodule/salesmodule71/SalesModule71.java
@@ -1,59 +1,56 @@
 /*
  * Copyright (c) 2011 - 2019, Apinauten GmbH
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, 
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this 
+ *  * Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.apiomat.nativemodule.salesmodule71;
 
 
+import com.apiomat.nativemodule.NativeModuleConfig;
+
 /**
- * Generated class for starting and stopping your module. 
- * 
- * Please be aware that all overridden methods in this class are called internally with 
- * A NEW INSTANCE OF THIS CLASS every time. That is, you cannot use instance variables 
+ * Generated class for starting and stopping your module.
+ *
+ * Please be aware that all overridden methods in this class are called internally with
+ * A NEW INSTANCE OF THIS CLASS every time. That is, you cannot use instance variables
  * in these overridden methods, because they are initialized again before each call.
- * 
+ *
  */
-@com.apiomat.nativemodule.Module( description="", 
+@com.apiomat.nativemodule.Module( description="",
          usedModules = { "Basics" }, securityPermissions = {} )
 public class SalesModule71 implements com.apiomat.nativemodule.IModule
 {
     static com.apiomat.nativemodule.IApplicationConfigProxy APP_CONFIG_PROXY;
     static com.apiomat.nativemodule.IStaticMethods AOM;
-    
-    // Sample for a module configuration
-    //
-    // @com.apiomat.nativemodule.NativeModuleConfig(
-    //     datatype = Type.TEXT,
-    //     example = "localhost",
-    //     title = "Server Hostname",
-    //     info = "Hostname of the server",
-    //     defaultValue = "localhost",
-    //     notifyAllNodes = true,
-    //     order = 1 )
-    // public static String HOSTNAME = "SalesModule71_hostname";
-    //
-    // Read @NativeModuleConfig values using the following code:
-    // SalesModule71.APP_CONFIG_PROXY.getConfigValue( SalesModule71.HOSTNAME, appName, system );
+
+    @com.apiomat.nativemodule.NativeModuleConfig(
+         datatype = NativeModuleConfig.Type.NUMBER,
+         example = "250",
+         title = "Lead Score",
+         info = "lead score static value",
+         defaultValue = "100",
+         notifyAllNodes = true,
+         order = 1 )
+    public static String DEFAULT_LEAD_SCORE = "SalesModule71_defaultLeadScore";
 
     /**
      * This method gets called once in the cluster when the module is uploaded.
@@ -72,7 +69,7 @@ public class SalesModule71 implements com.apiomat.nativemodule.IModule
     {
         //TODO will be called when service shuts down (maintenance)
     }
-    
+
     /**
      * This method gets called when the configuration for an app changes for this module
      */
@@ -81,20 +78,20 @@ public class SalesModule71 implements com.apiomat.nativemodule.IModule
     {
         // TODO Auto-generated method stub
     }
-    
+
     /**
      * This method may provide an implementation for your own REST interface
      */
     @Override
-    public com.apiomat.nativemodule.AbstractRestResource getSpecificRestResource( javax.ws.rs.core.UriInfo uriInfo, 
-    	javax.servlet.http.HttpServletRequest servletRequest, javax.ws.rs.core.SecurityContext securityContext, 
+    public com.apiomat.nativemodule.AbstractRestResource getSpecificRestResource( javax.ws.rs.core.UriInfo uriInfo,
+    	javax.servlet.http.HttpServletRequest servletRequest, javax.ws.rs.core.SecurityContext securityContext,
     	javax.ws.rs.core.Request wsRequest )
     {
         // TODO comment in to use a basic rest endpoint with a ping method
         //return new RestClass( uriInfo, servletRequest, securityContext, wsRequest );
         return null;
     }
-    
+
     /**
      * This method can be used for health checks (http://<hostname>/yambas/rest/modules/<modulename>/<appname>/health)
      * <p>
